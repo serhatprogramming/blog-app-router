08c855e55be098819f7615e070b1dc668473e102
serhatprogramming <serhatprogramming@gmail.com>
2023-09-03 03:30:16 -0400

    routes added.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import BlogList from "./views/BlogList";
+import UserList from "./views/UserList";
+import Login from "./views/Login";
+import About from "./views/About";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 const App = () => {
+  const menuStyle = {
+    display: "flex",
+    justifyContent: "space-between",
+    maxWidth: "200px",
+  };
   return (
-    <div>
-      <h2>Welcome to Blog Application</h2>
-      <BlogList />
-    </div>
+    <Router>
+      <div>
+        <div style={menuStyle}>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/users">Users</Link>
+          <Link to="/login">Login</Link>
+        </div>
+        <h2>Welcome to Blog Application</h2>
+        <Routes>
+          <Route path="/" element={<BlogList />} />
+          <Route path="/users" element={<UserList />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
